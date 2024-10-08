76a2a9fd55688ffb383007764323d2a72bb6d700
soumunda8 <soumunda8@naver.com>
2024-10-08 13:06:15 +0900

    CREATE : CONCERT RESERVATION API

---
+++ concert-reservation-api/src/main/java/io/hhplus/concert/ConcertReservationApiApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.concert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertReservationApiApplication.class, args);
+	}
+
+}
---
+++ concert-reservation-api/src/test/java/io/hhplus/concert/ConcertReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.concert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
