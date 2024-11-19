e325b68794f087b757c630b976edf22842be8eeb
soumunda8 <soumunda8@naver.com>
2024-11-20 05:19:42 +0900

    FEAT : kafka consumer, producer 설정 및 적용

--- concert-reservation-api/src/main/java/io/hhplus/concert/infrastructure/repository/messaging/kafka/KafkaProducerImp.java
+++
@@ -1,8 +0,0 @@
-package io.hhplus.concert.infrastructure.repository.messaging.kafka;
-
-import io.hhplus.concert.domain.messaging.kafka.KafkaProducer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class KafkaProducerImp implements KafkaProducer {
-}
